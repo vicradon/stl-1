6e2374fc5c24e0caa1d54b8d6ad4cb17f339c313
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2020-11-12 05:57:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,9 +20,7 @@ const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({
   env: {
-    AIRTABLE_API_KEY: "key5VOizF1IAFcKeD",
-    AIRTABLE_BASE_ID: "appf7Dc5IKom0wArV",
-    FAUNA_SERVER_SECRET: "fnAD26na28ACATnSJT0UD93sy_ft8gNx6Zt7gNFm",
+    FAUNA_SERVER_SECRET: "",
     PORT: 4532,
   },
 });
